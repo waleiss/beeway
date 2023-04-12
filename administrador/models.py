a11faeb7820ba9ee13b37d999ee69731aad562a5
Wallace <wlcs@ic.ufal.br>
2023-04-11 23:11:35 -0300

    model event melhorado

--- administrador/models.py
+++ administrador/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 class Event(models.Model):
     
-    nomeevento = models.CharField(max_length=50)
-    local = models.CharField(max_length=50)
-    datahora_inicio = models.DateTimeField()
-    datahora_termino = models.DateTimeField()
+    titulo = models.CharField(max_length=50)
+    local = models.CharField(max_length=150)
+    data_e_hora_inicio = models.DateTimeField()
+    data_e_hora_termino = models.DateTimeField()
     descricao = models.TextField()
+    def __str__(self):
+        return self.titulo
