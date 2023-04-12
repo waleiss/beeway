7ce0fb5f0342051670bc29298026cfe04475bf52
Wallace <wlcs@ic.ufal.br>
2023-04-12 19:39:46 -0300

    desc_evento view e template mudados, int = evento

--- administrador/views.py
+++ administrador/views.py
@@ -1,22 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Event
 
 def Login (request):
     return (render(request, 'pages/login.html'))
+
 def Cadastro(request):
     return (render(request, 'pages/cadastro.html'))
+
 def rSenha(request):
     return (render(request, 'pages/rSenha.html'))
+
 def Home(request):
     return (render(request, 'pages/home.html'))
+
 def todosEventos(request):
-    
     eventos = Event.objects.all()
     return (render(request, 'pages/todos.eventos.html', {'eventos': eventos}))
 
 def Sobre(request):
     return (render(request, 'pages/sobre.html'))
-def descEvento(request):
-    return (render(request, 'pages/desc_evento.html'))
+
+def descEvento(request, id):
+    evento = get_object_or_404(Event, pk=id)
+    return (render(request, 'pages/desc_evento.html', {'evento' : evento}))
+
 def Voucher(request):
     return (render(request, 'pages/voucher.html'))
